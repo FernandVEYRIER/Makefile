171ab5b9f4c6768af4096a156c6b7721f5ac60b4
FernandVEYRIER <fernand.veyrier@epitech.eu>
2015-01-05 13:13:39 +0100

    Added headers

--- Makefile.sh
+++ Makefile.sh
@@ -1,20 +1,57 @@
 #!/bin/bash
-## Makefile.sh for  in /home/veyrie_f/rendu/Piscine_C_J11/do-op
+## Makefile.sh for  in /home/veyrie_f/
 ## 
 ## Made by fernand veyrier
 ## Login   <veyrie_f@epitech.net>
 ## 
 ## Started on  Mon Oct 20 13:05:25 2014 fernand veyrier
-## Last update Mon Dec 22 17:36:44 2014 fernand veyrier
+## Last update Mon Jan  5 13:10:41 2015 fernand veyrier
 ##
 
-REVISION=1.7
+REVISION=1.8
+
+function include_header
+{
+    login=$(whoami)
+    name=$(cat /etc/passwd | grep `whoami` | grep -o -E ":[[:alpha:]' ']+:/" | tr -d ':' | tr -d '/')
+    date=$(date | grep -o -E "^[[:alpha:]' ']+[[:digit:]]+[[:digit:]' ':]+")
+    year=$(date | grep -o -E "[[:digit:]]+$")
+    echo "/*" >> ./include/my.h
+    echo "** Makefile for  in $PWD" >> ./include/my.h
+    echo "**" >> ./include/my.h
+    echo "** Made by $name" >> ./include/my.h
+    echo "** Login   <$login@epitech.net>" >> ./include/my.h
+    echo "**" >> ./include/my.h
+    echo "** Started on  $date$year $name" >> ./include/my.h
+    echo "** Last update $date$year $name" >> ./include/my.h
+    echo "*/" >> ./include/my.h
+    echo >> ./include/my.h
+}
+
+function makefile_header
+{
+    login=$(whoami)
+    name=$(cat /etc/passwd | grep `whoami` | grep -o -E ":[[:alpha:]' ']+:/" | tr -d ':' | tr -d '/')
+    date=$(date | grep -o -E "^[[:alpha:]' ']+[[:digit:]]+[[:digit:]' ':]+")
+    year=$(date | grep -o -E "[[:digit:]]+$")
+    echo "##" >> Makefile
+    echo "## Makefile for  in $PWD" >> Makefile 
+    echo "##" >> Makefile
+    echo "## Made by $name" >> Makefile
+    echo "## Login   <$login@epitech.net>" >> Makefile
+    echo "##" >> Makefile
+    echo "## Started on  $date$year $name" >> Makefile
+    echo "## Last update $date$year $name" >> Makefile
+    echo "##" >> Makefile
+    echo >> Makefile
+}
 
 function generate_makefile
 {
     if [ "$2" = "y" ] ; then
 	rm -f ./Makefile
 	touch ./Makefile
+	makefile_header
 	echo "CC		= gcc" >> Makefile
 	echo >> Makefile
 	echo "RM		= rm -f" >> Makefile
@@ -73,7 +110,9 @@ function create_files
     fi
     if [[ $2="y" ]]
     then
-        echo "#ifndef MY_H_" > ./include/my.h
+	rm ./include/my.h
+	include_header
+        echo "#ifndef MY_H_" >> ./include/my.h
         echo "# define MY_H_" >> ./include/my.h
 	ls -1 ./include/ | sed "s/^/#include \"/g" | sed "s/$/\"/g" | sed "s/#include \"my.h\"/$/g" | tr -d "$" > ./to_del.txt
 	sort ./to_del.txt | uniq >> ./include/my.h
